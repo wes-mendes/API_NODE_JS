47f775825007b19239d2db17ae0598f8868a7b46
wes-mendes <wesleymendesdesousa7@gmail.com>
2025-08-26 10:49:45 -0300

    COMMIT FINAL

--- server.js
+++ server.js
@@ -9,6 +9,38 @@ const app = express()
 app.use(express.json())
 
 //ROTAS
+
+app.put('/cadastro/:id', async (req,res)=>{
+
+    //console.log(req.params.id)
+    await prisma.usuario.update({
+        where:{
+            id: req.params.id
+        },
+        data:{
+            email: req.body.email, 
+            nome: req.body.nome,
+            idade: req.body.idade
+        }
+    })
+
+    res.status(201).json({"message":"Cliente atualizado!"})
+})
+
+app.delete('/cadastro/:id', async (req,res)=>{
+
+    //console.log(req.params.id)
+    await prisma.usuario.delete({
+        where:{
+            id: req.params.id
+        }
+    })
+
+
+    res.status(201).json({"message":"Cliente apagado com sucesso."})
+})
+
+
 app.get('/cadastro',async(req,res)=>{
 
     const lista_usuarios =  await prisma.usuario.findMany()
